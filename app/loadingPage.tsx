4b2e2b751459abb4fde65ed8090d5740762df6a3
Armandilo <muhammadarmand@student.usm.my>
2024-09-17 06:34:53 +0800

    loading changes

--- app/loadingPage.tsx
+++ app/loadingPage.tsx
@@ -3,12 +3,12 @@
 
 export default function LoadingPage() {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen gap-2">
         <div className='absolute inset-0 -z-8 opacity-5' style={{backgroundImage:`url(/grain.jpg)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}></div>
         <span className='sr-only'>Loading...</span>
-        <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-        <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-        <div className='h-8 w-8 bg-white rounded-full animate-bounce'></div>
+        <div className='h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+        <div className='h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+        <div className='h-4 w-4 bg-white rounded-full animate-bounce'></div>
       </div>
     );
   }
