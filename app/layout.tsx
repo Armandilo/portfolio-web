b251587f0a148f41563ba5c2aea00bed9ad97b38
Armandilo <muhammadarmand@student.usm.my>
2024-09-17 06:02:54 +0800

    window changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,12 +36,14 @@ export default function RootLayout({
     const handleMouseMove = (e: MouseEvent) => {
       setCursorPosition({ x: e.clientX, y: e.clientY });
     };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
+  
+    if (typeof window !== 'undefined') {
+      window.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
   }, []);
 
   if (!isMounted) {
