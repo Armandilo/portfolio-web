060d1461eea7902eea5828fd51b3d796ab3e2080
Armandilo <muhammadarmand@student.usm.my>
2024-09-17 11:24:31 +0800

    minor bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-
 import Header from "./components/Header/Header";
 import Highlight from "./components/Highlight/Highlight";
 import Repos from "./components/Repos/Repos";
 import Skills from "./components/Skills/Skills";
+
 import './app.css';
 
 export default function Home() {
+
   return (
-    <main className="snap-container overflow-y-scroll h-screen">
+    <main className="snap-container overflow-y-scroll scroll-smooth h-screen">
       <div className="snap-child md:h-auto lg:h-screen">
         <Header/>
       </div>
